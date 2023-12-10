808ab99bc1a8d6e1507d5be1c7e9f1742cf17136
joyfulviper <kimsk3113@naver.com>
2023-12-10 21:10:15 +0900

    chore: 프로젝트 초기 설정

---
+++ batch/src/test/java/com/garden/back/BatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garden.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
