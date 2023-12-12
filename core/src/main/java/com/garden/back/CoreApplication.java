b71bbf78206ae4f0c39de391ed0213726b1bc194
byeolhaha <j35635aa@gmail.com>
2023-12-13 04:15:41 +0900

    Style : 중복된 SpringbootConfiguration 제거

--- core/src/main/java/com/garden/back/CoreApplication.java
+++ core/src/main/java/com/garden/back/CoreApplication.java
@@ -3,7 +3,6 @@ package com.garden.back;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class CoreApplication {
 
     public static void main(String[] args) {
