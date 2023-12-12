caf6911d973899092afb7095fa238cf7d477b75b
byeolhaha <j35635aa@gmail.com>
2023-12-13 02:28:34 +0900

    Feat : 학원 이름 검색 관련 서비스 구현

---
+++ core/src/main/java/com/garden/back/garden/service/GardenReadService.java
@@ -0,0 +1,28 @@
+package com.garden.back.garden.service;
+
+import com.garden.back.garden.repository.GardenRepository;
+import com.garden.back.garden.service.dto.GardenByNameResults;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class GardenReadService {
+
+    private static final int GARDEN_BY_NAME_PAGE_SIZE = 10;
+    private final GardenRepository gardenRepository;
+
+    public GardenReadService(GardenRepository gardenRepository) {
+        this.gardenRepository = gardenRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public GardenByNameResults getGardensByName(String gardenName, int pageNumber) {
+        Pageable pageable = PageRequest.of(pageNumber, GARDEN_BY_NAME_PAGE_SIZE);
+
+        return GardenByNameResults.to(gardenRepository.findGardensByName(gardenName, pageable));
+
+    }
+
+}
