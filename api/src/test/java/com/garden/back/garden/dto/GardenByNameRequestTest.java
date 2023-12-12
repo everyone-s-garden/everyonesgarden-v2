9b08f06648695b80fb89168141b816867613211b
byeolhaha <j35635aa@gmail.com>
2023-12-13 04:14:58 +0900

    Test : 학원 이름 조회 요청 테스트 구현

---
+++ api/src/test/java/com/garden/back/garden/dto/GardenByNameRequestTest.java
@@ -0,0 +1,37 @@
+package com.garden.back.garden.dto;
+
+import com.garden.back.garden.dto.request.GardenByNameRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class GardenByNameRequestTest {
+
+    @DisplayName("GardenByNameRequest의 검증 테스트")
+    @ParameterizedTest
+    @MethodSource("provideGardenByNameRequestForValidation")
+    void gardenByNameRequest_validation(GardenByNameRequest request, boolean expectException) {
+        if (expectException) {
+            assertThrows(IllegalArgumentException.class, () -> GardenByNameRequest.to(request));
+        } else {
+            assertDoesNotThrow(() -> GardenByNameRequest.to(request));
+        }
+    }
+
+    private static Stream<Arguments> provideGardenByNameRequestForValidation() {
+        return Stream.of(
+                Arguments.of(new GardenByNameRequest("진겸이네 텃밭", 0), false),
+                Arguments.of(new GardenByNameRequest("도연이네 텃밭", 1), false),
+
+                Arguments.of(new GardenByNameRequest(null, 0), true),
+                Arguments.of(new GardenByNameRequest("", 0), true),
+                Arguments.of(new GardenByNameRequest("별이네 텃밭", -1), true)
+        );
+    }
+}
