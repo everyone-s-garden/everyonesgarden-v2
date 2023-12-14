c3b2eecafb544be5d5d2ae2fcce271757ddc92e8
byeolhaha <j35635aa@gmail.com>
2023-12-14 12:50:22 +0900

    Chore : 필요한 설정 추가

--- core/src/test/java/com/garden/back/garden/service/GardenReadServiceTest.java
+++ core/src/test/java/com/garden/back/garden/service/GardenReadServiceTest.java
@@ -24,7 +24,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.NONE;
 
 @Transactional
-@Import(TestDatabaseConfig.class)
 @SpringBootTest(webEnvironment = NONE)
 public class GardenReadServiceTest {
 
