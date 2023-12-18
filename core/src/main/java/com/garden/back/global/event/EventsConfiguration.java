2655b556dbb4484207d784b34eed4f6a08124e5a
joyfulviper <kimsk3113@naver.com>
2023-12-18 16:51:20 +0900

    feat: 신고 기능 추가

---
+++ core/src/main/java/com/garden/back/global/event/EventsConfiguration.java
@@ -0,0 +1,22 @@
+package com.garden.back.global.event;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class EventsConfiguration {
+
+    private final ApplicationContext applicationContext;
+
+    public EventsConfiguration(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public InitializingBean eventsInitializer() {
+        return () -> Events.setPublisher(applicationContext);
+    }
+
+}
--- core/src/main/java/com/garden/back/report/service/request/ReportGardenServiceRequest.java
+++ core/src/main/java/com/garden/back/report/service/request/ReportGardenServiceRequest.java
@@ -1,9 +1,14 @@
 package com.garden.back.report.service.request;
 
-import com.garden.back.report.ReportType;
+import com.garden.back.report.model.GardenReport;
+import com.garden.back.report.model.GardenReportType;
 
 public record ReportGardenServiceRequest(
     String content,
-    ReportType reportType
+    GardenReportType reportType
 ) {
+
+    public GardenReport toEntity(Long reporterId, Long gardenId) {
+        return GardenReport.create(reporterId, content, reportType, gardenId);
+    }
 }
