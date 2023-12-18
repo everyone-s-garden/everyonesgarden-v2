f7666748dfcfff88155346e636cbc5b303859ef6
byeolhaha <j35635aa@gmail.com>
2023-12-18 13:24:38 +0900

    Test : 해당 위치에 포함하는 랜덤한 point 좌표 반환하는 기능 구현

---
+++ core/src/test/java/com/garden/back/garden/service/RandomPointGenerator.java
@@ -0,0 +1,35 @@
+package com.garden.back.garden.service;
+
+import com.garden.back.garden.service.dto.request.GardenByComplexesParam;
+import com.garden.back.garden.util.GeometryUtil;
+import org.locationtech.jts.geom.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RandomPointGenerator {
+
+    private RandomPointGenerator() {
+        throw new RuntimeException("생성자를 통해서 객체를 생성할 수 없습니다.");
+    }
+    private static final int DEFAULT_COUNT = 5;
+
+    public static List<Point> generateRandomPoint(GardenByComplexesParam param) {
+        List<Point> coordinates = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i = 0; i < DEFAULT_COUNT; i++) {
+                double latitude = getRandomInRange(param.startLat(), param.endLat(), random);
+                double longitude = getRandomInRange(param.startLong(), param.endLong(), random);
+                coordinates.add(GeometryUtil.createPoint(latitude,longitude));
+        }
+        return coordinates;
+    }
+
+    private static double getRandomInRange(double min, double max, Random random) {
+        return min + (max - min) * random.nextDouble();
+    }
+
+
+}
