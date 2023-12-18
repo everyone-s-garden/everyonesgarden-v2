77915ca9560a3df84fb05ef25ecabf4e6b555c7f
byeolhaha <j35635aa@gmail.com>
2023-12-18 13:27:49 +0900

    Feat : 학원 위치 및 타입 검색 요청값에 대한 validation 구현 및 관련 dto 구현

---
+++ api/src/main/java/com/garden/back/garden/dto/request/GardenByComplexesRequest.java
@@ -0,0 +1,44 @@
+package com.garden.back.garden.dto.request;
+
+import com.garden.back.garden.dto.request.validation.ValidEnum;
+import com.garden.back.garden.service.dto.request.GardenByComplexesParam;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+
+public record GardenByComplexesRequest(
+        @NotBlank
+        @ValidEnum(enumClass = GardenType.class)
+        String gardenType,
+
+        @PositiveOrZero
+        Integer pageNumber,
+
+        @DecimalMin(value = "-90.0", message = "위도는 -90.0 보다 크거나 같아야 한다.")
+        @DecimalMax(value = "90.0", message = "위도는 90.0보다 같거나 작아야 한다.")
+        Double startLat,
+
+        @DecimalMin(value = "-180.0", message = "경도는 -180.0 보다 크거나 같아야 한다.")
+        @DecimalMax(value = "180.0", message = "경도는 180.0 보다 같거나 작아야 한다.")
+        Double startLong,
+
+        @DecimalMin(value = "-90.0", message = "위도는 -90.0 보다 크거나 같아야 한다.")
+        @DecimalMax(value = "90.0", message = "위도는 90.0보다 같거나 작아야 한다.")
+        Double endLat,
+
+        @DecimalMin(value = "-180.0", message = "경도는 -180.0 보다 크거나 같아야 한다.")
+        @DecimalMax(value = "180.0", message = "경도는 180.0 보다 같거나 작아야 한다.")
+        Double endLong
+) {
+    public static GardenByComplexesParam to(GardenByComplexesRequest request) {
+        return new GardenByComplexesParam(
+                request.gardenType(),
+                request.pageNumber(),
+                request.startLat(),
+                request.startLong(),
+                request.endLat(),
+                request.endLong()
+        );
+    }
+}
