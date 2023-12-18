c280a0a8fabaf0d8d95fa0a5767fec245d4db9f6
byeolhaha <j35635aa@gmail.com>
2023-12-18 13:26:24 +0900

    Feat : 텃밭 위치 및 타입에 따른 검색 컨트롤러 구현

--- api/src/main/java/com/garden/back/garden/GardenController.java
+++ api/src/main/java/com/garden/back/garden/GardenController.java
@@ -1,12 +1,15 @@
 package com.garden.back.garden;
 
 import com.garden.back.garden.dto.request.GardenByNameRequest;
+import com.garden.back.garden.dto.request.GardenByComplexesRequest;
 import com.garden.back.garden.dto.request.GardenGetAllRequest;
+import com.garden.back.garden.dto.response.GardenByComplexesResponses;
 import com.garden.back.garden.dto.response.GardenByNameResponses;
 import com.garden.back.garden.dto.response.GardenGetAllResponses;
 import com.garden.back.garden.service.GardenReadService;
 import com.garden.back.garden.service.dto.request.GardenByNameParam;
 import com.garden.back.garden.service.dto.request.GardenGetAllParam;
+import com.garden.back.garden.service.dto.response.GardenByComplexesResults;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -45,6 +48,20 @@ public class GardenController {
         return ResponseEntity.status(HttpStatus.OK)
                 .body(GardenGetAllResponses.to(
                         gardenReadService.getAllGarden(gardenGetAllParam)));
+    }
+
+    @GetMapping(
+            path = "/by-complexes",
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<GardenByComplexesResponses> getGardensByComplexes(
+            @Valid @ModelAttribute GardenByComplexesRequest request) {
+        GardenByComplexesResults gardensByComplexes
+                = gardenReadService.getGardensByComplexes(GardenByComplexesRequest.to(request));
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(GardenByComplexesResponses.to(gardensByComplexes));
+
     }
 
 }
