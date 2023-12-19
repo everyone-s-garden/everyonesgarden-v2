4426f21c00363d619c56b74aca5ebf28e9bd8c8a
byeolhaha <j35635aa@gmail.com>
2023-12-20 02:38:49 +0900

    Test : 중복 및 최신 텃밭에 대한 검증 테스트 추가

--- core/src/test/java/com/garden/back/garden/service/recentview/RecentViewGardensTest.java
+++ core/src/test/java/com/garden/back/garden/service/recentview/RecentViewGardensTest.java
@@ -1,5 +1,6 @@
 package com.garden.back.garden.service.recentview;
 
+import com.garden.back.testutil.garden.GardenFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -25,5 +26,22 @@ public class RecentViewGardensTest {
         // Then
         List<RecentViewGarden> recentViewGardenList = recentViews.getRecentViewGardens().recentViewGardens().stream().toList();
         assertThat(recentViewGardenList.size()).isEqualTo(RecentViewGardens.MAX_RECENT_VIEW_COUNT);
+    }
+
+    @DisplayName("중복된 텃밭 정보가 저장되어도 최근의 하나만 저장됨을 확인할 수 있다.")
+    @Test
+    void addRecentViewGarden_distinctGarden() {
+        // Given
+        RecentViewGardens recentViews = new RecentViewGardens(new ArrayDeque<>());
+
+        // When
+        recentViews.addRecentViewGarden(GardenFixture.publicRecentViewGarden());
+        recentViews.addRecentViewGarden(GardenFixture.privateRecentViewGarden());
+        recentViews.addRecentViewGarden(GardenFixture.publicRecentViewGarden());
+
+        // Then
+        List<RecentViewGarden> recentViewGardenList = recentViews.getRecentViewGardens().recentViewGardens().stream().toList();
+        assertThat(recentViewGardenList).
+                containsExactly(GardenFixture.publicRecentViewGarden(),GardenFixture.privateRecentViewGarden());
     }
 }
