12a1afaab8447964e388433efd96bed219c0dd2f
byeolhaha <j35635aa@gmail.com>
2023-12-20 02:40:44 +0900

    Feat : 최근 본 텃밭 서비스 관련 dto 구현

---
+++ core/src/main/java/com/garden/back/garden/service/dto/response/RecentGardenResults.java
@@ -0,0 +1,40 @@
+package com.garden.back.garden.service.dto.response;
+
+import com.garden.back.garden.service.recentview.RecentViewGarden;
+import com.garden.back.garden.service.recentview.RecentViewGardens;
+
+import java.util.List;
+
+public record RecentGardenResults(
+        List<RecentGardenResult> recentGardenResults
+) {
+    public static RecentGardenResults to(RecentViewGardens recentViewGardens) {
+        return new RecentGardenResults(
+                recentViewGardens.getRecentViewGardens().recentViewGardens().stream()
+                        .map(RecentGardenResult::to)
+                        .toList()
+        );
+    }
+
+    public record RecentGardenResult(
+            Long gardenId,
+            String size,
+            String gardenName,
+            String price,
+            String images,
+            String gardenStatus,
+            String gardenType
+    ) {
+        public static RecentGardenResult to(RecentViewGarden recentViewGarden) {
+            return new RecentGardenResult(
+                    recentViewGarden.gardenId(),
+                    recentViewGarden.size(),
+                    recentViewGarden.gardenName(),
+                    recentViewGarden.price(),
+                    recentViewGarden.images(),
+                    recentViewGarden.gardenStatus(),
+                    recentViewGarden.gardenType()
+            );
+        }
+    }
+}
