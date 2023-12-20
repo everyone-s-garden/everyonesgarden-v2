7630d567bab57f6cb8cad13cccc9f44f6dc86920
byeolhaha <j35635aa@gmail.com>
2023-12-20 16:28:05 +0900

    Feat : 좋아요한 텃밭 조회 서비스 구현

--- core/src/main/java/com/garden/back/garden/service/GardenReadService.java
+++ core/src/main/java/com/garden/back/garden/service/GardenReadService.java
@@ -1,19 +1,15 @@
 package com.garden.back.garden.service;
 
-import com.garden.back.garden.model.Garden;
 import com.garden.back.garden.repository.garden.GardenRepository;
 import com.garden.back.garden.repository.garden.dto.GardenGetAll;
 import com.garden.back.garden.repository.garden.dto.GardensByComplexes;
 import com.garden.back.garden.repository.garden.dto.response.GardenDetailRepositoryResponse;
-import com.garden.back.garden.repository.garden.dto.response.GardenMineRepositoryResponse;
-import com.garden.back.garden.repository.gardenimage.GardenImageRepository;
 import com.garden.back.garden.service.dto.request.*;
 import com.garden.back.garden.service.dto.response.*;
 import com.garden.back.garden.service.recentview.GardenHistoryManager;
 import com.garden.back.garden.service.recentview.RecentViewGarden;
 import com.garden.back.garden.util.PageMaker;
 import org.springframework.data.domain.Slice;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -68,6 +64,11 @@ public class GardenReadService {
 
     public GardenMineResults getMyGarden(Long memberId) {
         return GardenMineResults.to(gardenRepository.findByWriterId(memberId));
+    }
+
+    public GardenLikeByMemberResults getLikeGardensByMember(Long memberId) {
+        return GardenLikeByMemberResults.to(
+                gardenRepository.getLikeGardenByMember(memberId));
     }
 
 }
