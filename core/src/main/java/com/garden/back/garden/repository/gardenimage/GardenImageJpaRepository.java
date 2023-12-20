8d26058b4f6e8708111359dc7b013781a2f76009
byeolhaha <j35635aa@gmail.com>
2023-12-20 14:05:30 +0900

    Feat : 이미지 삭제 쿼리 구현

--- core/src/main/java/com/garden/back/garden/repository/gardenimage/GardenImageJpaRepository.java
+++ core/src/main/java/com/garden/back/garden/repository/gardenimage/GardenImageJpaRepository.java
@@ -2,6 +2,22 @@ package com.garden.back.garden.repository.gardenimage;
 
 import com.garden.back.garden.model.GardenImage;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface GardenImageJpaRepository extends JpaRepository<GardenImage, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Query(
+            "delete from GardenImage as gi where gi.garden.gardenId =:gardenId"
+    )
+    void deleteByGardenId(@Param("gardenId") Long gardenId);
+
+    @Query(
+            "select gi from GardenImage as gi where gi.garden.gardenId =:gardenId"
+    )
+    Optional<GardenImage> findByGardenId(@Param("gardenId") Long gardenId);
 }
