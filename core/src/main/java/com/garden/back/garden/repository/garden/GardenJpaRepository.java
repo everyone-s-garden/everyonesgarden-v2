cea1dccb911c5449e7a6d55cc9aaf9b851068b59
byeolhaha <j35635aa@gmail.com>
2023-12-20 14:26:12 +0900

    Style : 불필요한 import 삭제

--- core/src/main/java/com/garden/back/garden/repository/garden/GardenJpaRepository.java
+++ core/src/main/java/com/garden/back/garden/repository/garden/GardenJpaRepository.java
@@ -4,15 +4,12 @@ import com.garden.back.garden.model.Garden;
 import com.garden.back.garden.repository.garden.dto.GardenByName;
 import com.garden.back.garden.repository.garden.dto.GardenGetAll;
 import com.garden.back.garden.repository.garden.dto.response.GardenDetailRepositoryResponse;
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import java.util.List;
-import java.util.Optional;
 
 public interface GardenJpaRepository extends JpaRepository<Garden, Long>, GardenCustomRepository, GardenRepository {
 
