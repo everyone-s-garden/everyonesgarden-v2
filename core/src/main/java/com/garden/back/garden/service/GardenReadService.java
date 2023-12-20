23bbbb12a4908ddb46547cf3079eb77fe05f5d8a
byeolhaha <j35635aa@gmail.com>
2023-12-20 14:04:49 +0900

    Feat : 텃밭 삭제 서비스 로직 구현

--- core/src/main/java/com/garden/back/garden/service/GardenReadService.java
+++ core/src/main/java/com/garden/back/garden/service/GardenReadService.java
@@ -1,27 +1,32 @@
 package com.garden.back.garden.service;
 
+import com.garden.back.garden.model.Garden;
 import com.garden.back.garden.repository.garden.GardenRepository;
 import com.garden.back.garden.repository.garden.dto.GardenGetAll;
 import com.garden.back.garden.repository.garden.dto.GardensByComplexes;
 import com.garden.back.garden.repository.garden.dto.response.GardenDetailRepositoryResponse;
+import com.garden.back.garden.repository.gardenimage.GardenImageRepository;
 import com.garden.back.garden.service.dto.request.*;
 import com.garden.back.garden.service.dto.response.*;
 import com.garden.back.garden.service.recentview.GardenHistoryManager;
 import com.garden.back.garden.service.recentview.RecentViewGarden;
-import com.garden.back.garden.service.recentview.RecentViewGardens;
 import com.garden.back.garden.util.PageMaker;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Objects;
 
 @Service
 public class GardenReadService {
     private final GardenRepository gardenRepository;
+    private final GardenImageRepository gardenImageRepository;
     private final GardenHistoryManager gardenHistoryManager;
 
-    public GardenReadService(GardenRepository gardenRepository, GardenHistoryManager gardenHistoryManager) {
+    public GardenReadService(GardenRepository gardenRepository, GardenImageRepository gardenImageRepository, GardenHistoryManager gardenHistoryManager) {
         this.gardenRepository = gardenRepository;
+        this.gardenImageRepository = gardenImageRepository;
         this.gardenHistoryManager = gardenHistoryManager;
     }
 
@@ -62,5 +67,19 @@ public class GardenReadService {
     public RecentGardenResults getRecentGardens(Long memberId) {
         return RecentGardenResults.to(gardenHistoryManager.getRecentViewGarden(memberId));
     }
+
+    @Transactional
+    public void deleteGarden(GardenDeleteParam param) {
+        Garden gardenToDelete = gardenRepository.getById(param.gardenId());
+
+        if (!Objects.equals(gardenToDelete.getWriterId(), param.memberId())) {
+            throw new IllegalArgumentException("텃밭 게시글 작성자만 텃밭을 삭제할 수 있습니다.");
+        }
+
+        gardenImageRepository.deleteByGardenId(param.gardenId());
+        gardenRepository.deleteById(param.gardenId());
+
+    }
+
 
 }
