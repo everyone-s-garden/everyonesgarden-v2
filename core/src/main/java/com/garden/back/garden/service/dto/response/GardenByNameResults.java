89beaa2287e98a89b2dcfd2433c35c112072ac26
byeolhaha <j35635aa@gmail.com>
2023-12-22 20:51:49 +0900

    refactor : slice에서 필요한 정보만 파싱해서 list로 반환

--- core/src/main/java/com/garden/back/garden/service/dto/response/GardenByNameResults.java
+++ core/src/main/java/com/garden/back/garden/service/dto/response/GardenByNameResults.java
@@ -3,13 +3,18 @@ package com.garden.back.garden.service.dto.response;
 import com.garden.back.garden.repository.garden.dto.GardenByName;
 import org.springframework.data.domain.Slice;
 
+import java.util.List;
+
 public record GardenByNameResults(
-        Slice<GardenByNameResult> gardensByName
+        List<GardenByNameResult> gardensByName,
+        boolean hasNext
 ) {
 
     public static GardenByNameResults to(Slice<GardenByName> gardensByName) {
         return new GardenByNameResults(
-                gardensByName.map(GardenByNameResult::to));
+                gardensByName.map(GardenByNameResult::to)
+                        .stream().toList(),
+                gardensByName.hasNext());
     }
 
     public record GardenByNameResult(
