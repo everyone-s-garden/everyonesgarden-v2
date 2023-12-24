7aadced8b12058c942433c63e99a4bc3918ae2aa
Jinkyum Park <jinpark1025@gmail.com>
2023-12-24 14:42:36 +0900

    feat: (알림 시스템) EmailJavaNotificationService open으로 변경

--- core/src/main/java/com/garden/back/notification/service/EmailJavaNotificationService.kt
+++ core/src/main/java/com/garden/back/notification/service/EmailJavaNotificationService.kt
@@ -9,7 +9,7 @@ import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 
 @Service
-class EmailJavaNotificationService(
+open class EmailJavaNotificationService(
     @Value("example@gmail.com")
     private var OFFICIAL_EMAIL: String,
 
