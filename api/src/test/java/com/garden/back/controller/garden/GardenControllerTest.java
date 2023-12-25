5acfb4e10c52014e9048fa773360badd7fd31c95
byeolhaha <j35635aa@gmail.com>
2023-12-25 20:21:28 +0900

    test : 텃밭 등록 컨트롤러 요청값에 대한 컨트롤러 테스트 구현

--- api/src/test/java/com/garden/back/controller/garden/GardenControllerTest.java
+++ api/src/test/java/com/garden/back/controller/garden/GardenControllerTest.java
@@ -1,15 +1,18 @@
 package com.garden.back.controller.garden;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import com.garden.back.garden.dto.request.GardenByComplexesRequest;
 import com.garden.back.ControllerTestSupport;
+import com.garden.back.garden.dto.request.GardenByComplexesRequest;
 import com.garden.back.garden.dto.request.GardenByNameRequest;
+import com.garden.back.garden.dto.request.GardenCreateRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class GardenControllerTest extends ControllerTestSupport {
 
@@ -30,6 +33,16 @@ class GardenControllerTest extends ControllerTestSupport {
                         .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().is4xxClientError());
     }
+
+    @DisplayName("분양하고자 하는 텃밭을 등록할 때 요청값에 대해서 검증한다.")
+    @ParameterizedTest
+    @MethodSource("provideInvalidGardenCreateRequest")
+    void createGarden_invalidRequest(GardenCreateRequest request) throws Exception {
+        mockMvc.perform(post("/v2/gardens")
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().is4xxClientError());
+    }
+
 
     private static Stream<GardenByNameRequest> provideInvalidGardenByNameRequest() {
         return Stream.of(
@@ -96,4 +109,161 @@ class GardenControllerTest extends ControllerTestSupport {
         );
     }
 
+    private static Stream<GardenCreateRequest> provideInvalidGardenCreateRequest() {
+        return Stream.of(
+                new GardenCreateRequest(
+                        "별이네 텃밭",
+                        "100",
+                        "200",
+                        "aactive",
+                        "www.everygarden.me",
+                        "000-000-0000",
+                        "인천광역시 서구 만수동 200",
+                        37.444917,
+                        127.138868,
+                                true,
+                                true,
+                                true,
+                        "화장실이 깨끗하고 흙이 좋아요",
+                        "2023.12.01",
+                        "2023.12.23",
+                        "2023.12.01",
+                        "2023.12.31"
+                ),
+                new GardenCreateRequest(
+                        "별이네 텃밭",
+                        "100",
+                        "200",
+                        "ACTIVE",
+                        "www.everygarden.me",
+                        "000-000-0000",
+                        "인천광역시 서구 만수동 200",
+                        137.444917,
+                        127.138868,
+                        true,
+                        true,
+                        true,
+                        "화장실이 깨끗하고 흙이 좋아요",
+                        "2023.12.01",
+                        "2023.12.23",
+                        "2023.12.01",
+                        "2023.12.31"
+                ),
+                new GardenCreateRequest(
+                        "별이네 텃밭",
+                        "100",
+                        "200",
+                        "ACTIVE",
+                        "www.everygarden.me",
+                        "000-000-0000",
+                        "인천광역시 서구 만수동 200",
+                        37.444917,
+                        190.138868,
+                        true,
+                        true,
+                        true,
+                        "화장실이 깨끗하고 흙이 좋아요",
+                        "2023.12.01",
+                        "2023.12.23",
+                        "2023.12.01",
+                        "2023.12.31"
+                ),
+                new GardenCreateRequest(
+                        "별이네 텃밭",
+                        "100",
+                        "200",
+                        "ACTIVE",
+                        "www.everygarden.me",
+                        "000-000-0000",
+                        "인천광역시 서구 만수동 200",
+                        37.444917,
+                        127.138868,
+                        true,
+                        true,
+                        true,
+                        "화장실이 깨끗하",
+                        "2023.12.01",
+                        "2023.12.23",
+                        "2023.12.01",
+                        "2023.12.31"
+                ),
+                new GardenCreateRequest(
+                        "별이네 텃밭",
+                        "100",
+                        "200",
+                        "ACTIVE",
+                        "www.everygarden.me",
+                        "000-000-0000",
+                        "인천광역시 서구 만수동 200",
+                        37.444917,
+                        127.138868,
+                        true,
+                        true,
+                        true,
+                        "화장실이 깨끗하고 흙이 좋아요",
+                        "2023-12-01",
+                        "2023-12-23",
+                        "2023.12.01",
+                        "2023.12.31"
+                ),
+                new GardenCreateRequest(
+                        "별이네 텃밭",
+                        "100",
+                        "200",
+                        "ACTIVE",
+                        "www.everygarden.me",
+                        "000-000-0000",
+                        "인천광역시 서구 만수동 200",
+                        37.444917,
+                        127.138868,
+                        true,
+                        true,
+                        true,
+                        "화장실이 깨끗하고 흙이 좋아요",
+                        "2023.12.01",
+                        "2023.11.23",
+                        "2023.12.01",
+                        "2023.12.31"
+                )
+        );
+    }
+
 }
+//@NotBlank
+//String gardenName,
+//@NotBlank
+//String price,
+//@NotBlank
+//String size,
+//@NotBlank
+//@EnumValue(enumClass = GardenStatus.class)
+//String gardenStatus,
+//String linkForRequest,
+//String contact,
+//@NotBlank
+//String address,
+//@NotBlank
+//@DecimalMin(value = "-90.0", message = "위도는 -90.0 보다 크거나 같아야 한다.")
+//@DecimalMax(value = "90.0", message = "위도는 90.0보다 같거나 작아야 한다.")
+//Double latitude,
+//@NotBlank
+//@DecimalMin(value = "-180.0", message = "경도는 -180.0 보다 크거나 같아야 한다.")
+//@DecimalMax(value = "180.0", message = "경도는 180.0 보다 같거나 작아야 한다.")
+//Double longitude,
+//@NotNull
+//Boolean isToilet,
+//@NotNull
+//Boolean isWaterway,
+//@NotNull
+//Boolean isEquipment,
+//@NotBlank
+//@Length(min = 10, message = "텃밭 설명은 최소 10글자입니다.")
+//String gardenDescription,
+//@ValidDate
+//String recruitStartDate,
+//@ValidDate
+//String recruitEndDate,
+//@ValidDate
+//String useStartDate,
+//@ValidDate
+//String useEndDate
