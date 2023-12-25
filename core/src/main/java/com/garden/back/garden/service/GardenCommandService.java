5bf73fb25c5055042cb40e220a58aa48a87a224e
byeolhaha <j35635aa@gmail.com>
2023-12-25 20:17:07 +0900

    feat : 텃밭 등록 서비스 구현

--- core/src/main/java/com/garden/back/garden/service/GardenCommandService.java
+++ core/src/main/java/com/garden/back/garden/service/GardenCommandService.java
@@ -1,28 +1,34 @@
 package com.garden.back.garden.service;
 
 import com.garden.back.garden.model.Garden;
+import com.garden.back.garden.model.GardenImage;
 import com.garden.back.garden.model.GardenLike;
 import com.garden.back.garden.repository.garden.GardenRepository;
 import com.garden.back.garden.repository.gardenimage.GardenImageRepository;
 import com.garden.back.garden.repository.gardenlike.GardenLikeRepository;
+import com.garden.back.garden.service.dto.request.GardenCreateParam;
 import com.garden.back.garden.service.dto.request.GardenDeleteParam;
 import com.garden.back.garden.service.dto.request.GardenLikeCreateParam;
 import com.garden.back.garden.service.dto.request.GardenLikeDeleteParam;
+import com.garden.back.global.image.ParallelImageUploader;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Objects;
+import java.util.List;
 
 @Service
 public class GardenCommandService {
+    private static final String GARDEN_IMAGE_DIRECTORY = "garden/";
     private final GardenRepository gardenRepository;
     private final GardenImageRepository gardenImageRepository;
     private final GardenLikeRepository gardenLikeRepository;
+    private final ParallelImageUploader parallelImageUploader;
 
-    public GardenCommandService(GardenRepository gardenRepository, GardenImageRepository gardenImageRepository, GardenLikeRepository gardenLikeRepository) {
+    public GardenCommandService(GardenRepository gardenRepository, GardenImageRepository gardenImageRepository, GardenLikeRepository gardenLikeRepository, ParallelImageUploader parallelImageUploader) {
         this.gardenRepository = gardenRepository;
         this.gardenImageRepository = gardenImageRepository;
         this.gardenLikeRepository = gardenLikeRepository;
+        this.parallelImageUploader = parallelImageUploader;
     }
 
     @Transactional
@@ -44,6 +50,15 @@ public class GardenCommandService {
 
     public void deleteGardenLike(GardenLikeDeleteParam param) {
         gardenLikeRepository.delete(param.memberId(), param.gardenId());
+    }
+
+    public Long createGarden(GardenCreateParam param) {
+        Garden savedGarden = gardenRepository.save(GardenCreateParam.toEntity(param));
+
+        List<String> uploadImageUrls = parallelImageUploader.upload(GARDEN_IMAGE_DIRECTORY, param.gardenImages());
+        uploadImageUrls.forEach(uploadImageUrl -> gardenImageRepository.save(GardenImage.of(uploadImageUrl, savedGarden)));
+
+        return savedGarden.getGardenId();
     }
 
 }
