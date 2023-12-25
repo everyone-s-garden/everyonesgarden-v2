781ca77f9c64da53bb2a8529d302ee45e3442816
byeolhaha <j35635aa@gmail.com>
2023-12-25 20:23:16 +0900

    style : LocalDate로 타입 변경에 따른 수정

--- core/src/test/java/com/garden/back/testutil/garden/GardenFixture.java
+++ core/src/test/java/com/garden/back/testutil/garden/GardenFixture.java
@@ -4,12 +4,16 @@ import com.garden.back.garden.model.Garden;
 import com.garden.back.garden.model.vo.GardenStatus;
 import com.garden.back.garden.model.vo.GardenType;
 import com.garden.back.garden.service.dto.request.GardenByComplexesParam;
+import com.garden.back.garden.service.dto.request.GardenCreateParam;
 import com.garden.back.garden.service.dto.request.GardenDetailParam;
 import com.garden.back.garden.service.recentview.RecentViewGarden;
 import com.garden.back.global.GeometryUtil;
 import org.locationtech.jts.geom.Point;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.util.List;
 
 public class GardenFixture {
 
@@ -33,10 +37,10 @@ public class GardenFixture {
                 "000-000-000",
                 "200.23",
                 "화장실이 깨끗하고 농기구를 빌려줍니다.",
-                LocalDateTime.now(),
-                LocalDateTime.now().plusDays(30),
-                LocalDateTime.now().plusDays(31),
-                LocalDateTime.now().plusMonths(6),
+                LocalDate.now(),
+                LocalDate.now().plusDays(30),
+                LocalDate.now().plusDays(31),
+                LocalDate.now().plusMonths(6),
                 true,
                 false,
                 true,
@@ -93,10 +97,10 @@ public class GardenFixture {
                 "000-000-000",
                 "200",
                 "화장실이 깨끗해요 그리고 시설이 최신식입니다.",
-                LocalDateTime.now(),
-                LocalDateTime.now().plusDays(30),
-                LocalDateTime.now().plusDays(31),
-                LocalDateTime.now().plusMonths(6),
+                LocalDate.now(),
+                LocalDate.now().plusDays(30),
+                LocalDate.now().plusDays(31),
+                LocalDate.now().plusMonths(6),
                 true,
                 false,
                 true,
@@ -134,6 +138,34 @@ public class GardenFixture {
                 "10000",
                 GardenStatus.ACTIVE.name(),
                 GardenType.PRIVATE.name()
+        );
+    }
+
+    public static GardenCreateParam gardenCreateParam(String expectedUrl) {
+        MultipartFile multipartFile = new MockMultipartFile("test", expectedUrl.getBytes());
+
+        return new GardenCreateParam(
+                List.of(multipartFile),
+                "별이네 텃밭",
+                "100",
+                "200",
+                GardenStatus.ACTIVE,
+                "www.everygarden.me",
+                "000-000-0000",
+                "인천광역시 서구 만수동 200",
+                LATITUDE,
+                LONGITUDE,
+                new GardenCreateParam.GardenFacility(
+                        true,
+                        true,
+                        true
+                ),
+                "화장실이 깨끗하고 흙이 좋아요",
+                LocalDate.now(),
+                LocalDate.now().plusDays(30),
+                LocalDate.now().plusDays(31),
+                LocalDate.now().plusMonths(6),
+                1L
         );
     }
 
