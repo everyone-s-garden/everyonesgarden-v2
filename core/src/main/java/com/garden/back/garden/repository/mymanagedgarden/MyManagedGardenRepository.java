5d7c2ce300721fcadd4515a19ae86314efcc5532
byeolhaha <j35635aa@gmail.com>
2023-12-27 01:57:50 +0900

    feat : 내가 가꾸는 텃밭 조회 메서드 추가

--- core/src/main/java/com/garden/back/garden/repository/mymanagedgarden/MyManagedGardenRepository.java
+++ core/src/main/java/com/garden/back/garden/repository/mymanagedgarden/MyManagedGardenRepository.java
@@ -1,11 +1,17 @@
 package com.garden.back.garden.repository.mymanagedgarden;
 
+import com.garden.back.garden.domain.MyManagedGarden;
 import com.garden.back.garden.repository.mymanagedgarden.dto.MyManagedGardenGetRepositoryResponse;
+import jakarta.persistence.EntityNotFoundException;
 
 import java.util.List;
 
 public interface MyManagedGardenRepository extends MyManagedGardenJpaRepository {
 
     List<MyManagedGardenGetRepositoryResponse> getByMemberId(Long memberId);
+
+    default MyManagedGarden getById(Long myManagedGardenId) {
+        return findById(myManagedGardenId).orElseThrow(() -> new EntityNotFoundException("존재하지 않은 자원입니다."));
+    }
 
 }
