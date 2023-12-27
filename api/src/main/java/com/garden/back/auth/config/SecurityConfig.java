481f1d547848cf5a3057249bc0bb3f64abbef47c
joyfulviper <kimsk3113@naver.com>
2023-12-28 03:54:52 +0900

    fix: 테스트 코드에서 설정 파일 받을 수 있도록 수정

--- api/src/main/java/com/garden/back/auth/config/SecurityConfig.java
+++ api/src/main/java/com/garden/back/auth/config/SecurityConfig.java
@@ -39,7 +39,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000", "https://everyonesgarden.vercel.app/"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH", "DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
