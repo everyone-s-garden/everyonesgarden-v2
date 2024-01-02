13c39af81fc1e48bb3fc10aceead29f0a6a9c670
byeolhaha <j35635aa@gmail.com>
2024-01-03 01:53:39 +0900

    teat : 작물 거래 Rest docs 테스트 구현

--- chat/src/test/java/com/garden/back/docs/ChatDocsTest.java
+++ chat/src/test/java/com/garden/back/docs/ChatDocsTest.java
@@ -1,8 +1,10 @@
 package com.garden.back.docs;
 
 import com.garden.back.controller.ChatRoomController;
+import com.garden.back.controller.dto.CropChatRoomCreateRequest;
 import com.garden.back.controller.dto.GardenChatRoomCreateRequest;
-import com.garden.back.service.ChatRoomService;
+import com.garden.back.service.crop.CropChatRoomService;
+import com.garden.back.service.garden.GardenChatRoomService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
@@ -22,18 +24,19 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 class ChatDocsTest extends RestDocsSupport {
 
-    ChatRoomService chatRoomService = mock(ChatRoomService.class);
+    GardenChatRoomService gardenChatRoomService = mock(GardenChatRoomService.class);
+    CropChatRoomService cropChatRoomService = mock(CropChatRoomService.class);
 
     @Override
     protected Object initController() {
-        return new ChatRoomController(chatRoomService);
+        return new ChatRoomController(gardenChatRoomService, cropChatRoomService);
     }
 
     @DisplayName("텃밭 분양 관련 채팅방을 생성할 수 있다.")
     @Test
-    void createChatRoom() throws Exception {
+    void createGardenChatRoom() throws Exception {
         GardenChatRoomCreateRequest gardenChatRoomCreateRequest = ChatRoomFixture.chatRoomCreateRequest();
-        given(chatRoomService.createGardenChatRoom(any())).willReturn(1L);
+        given(gardenChatRoomService.createGardenChatRoom(any())).willReturn(1L);
 
         mockMvc.perform(post("/chats/gardens")
                         .contentType(MediaType.APPLICATION_JSON)
@@ -43,8 +46,28 @@ class ChatDocsTest extends RestDocsSupport {
                 .andDo(document("create-garden-chat-room",
                         requestFields(
                                 fieldWithPath("writerId").type(JsonFieldType.NUMBER).description("본 게시글의 작성자 아이디"),
-                                fieldWithPath("postId").type(JsonFieldType.NUMBER).description("본 게시글 아이디"),
-                                fieldWithPath("chatType").type(JsonFieldType.STRING).description("채팅 타입 : GARDEN(텃밭 분양), HARVEST(농작물 거래)")
+                                fieldWithPath("postId").type(JsonFieldType.NUMBER).description("본 게시글 아이디")
+                        ),
+                        responseHeaders(
+                                headerWithName("Location").description("생성된 채팅방 id를 포함한 url")
+                        )));
+    }
+
+    @DisplayName("작물 거래 관련 채팅방을 생성할 수 있다.")
+    @Test
+    void createCropChatRoom() throws Exception {
+        CropChatRoomCreateRequest cropChatRoomCreateRequest = ChatRoomFixture.chatRoomCropCreateRequest();
+        given(cropChatRoomService.createCropChatRoom(any())).willReturn(1L);
+
+        mockMvc.perform(post("/chats/crops")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(cropChatRoomCreateRequest)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andDo(document("create-crop-chat-room",
+                        requestFields(
+                                fieldWithPath("writerId").type(JsonFieldType.NUMBER).description("본 게시글의 작성자 아이디"),
+                                fieldWithPath("postId").type(JsonFieldType.NUMBER).description("본 게시글 아이디")
                         ),
                         responseHeaders(
                                 headerWithName("Location").description("생성된 채팅방 id를 포함한 url")
--- chat/src/test/java/com/garden/back/docs/ChatRoomFixture.java
+++ chat/src/test/java/com/garden/back/docs/ChatRoomFixture.java
@@ -1,5 +1,6 @@
 package com.garden.back.docs;
 
+import com.garden.back.controller.dto.CropChatRoomCreateRequest;
 import com.garden.back.controller.dto.GardenChatRoomCreateRequest;
 
 public class ChatRoomFixture {
@@ -7,8 +8,15 @@ public class ChatRoomFixture {
     public static GardenChatRoomCreateRequest chatRoomCreateRequest() {
         return new GardenChatRoomCreateRequest(
                 1L,
-                1L,
-                "GARDEN"
+                1L
         );
     }
+
+    public static CropChatRoomCreateRequest chatRoomCropCreateRequest() {
+        return new CropChatRoomCreateRequest(
+                1L,
+                1L
+        );
+    }
+
 }
