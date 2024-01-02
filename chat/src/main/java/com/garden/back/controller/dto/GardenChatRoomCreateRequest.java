3bc22e5a36cafdcc4a416ead9d1c377c7908503e
byeolhaha <j35635aa@gmail.com>
2024-01-03 01:54:22 +0900

    style : 불필요한 ChatType 필드 삭제

--- chat/src/main/java/com/garden/back/controller/dto/GardenChatRoomCreateRequest.java
+++ chat/src/main/java/com/garden/back/controller/dto/GardenChatRoomCreateRequest.java
@@ -1,8 +1,6 @@
 package com.garden.back.controller.dto;
 
-import com.garden.back.domain.ChatType;
 import com.garden.back.global.loginuser.LoginUser;
-import com.garden.back.global.validation.EnumValue;
 import com.garden.back.service.dto.request.GardenChatRoomCreateParam;
 import jakarta.validation.constraints.Positive;
 
@@ -11,18 +9,14 @@ public record GardenChatRoomCreateRequest(
         Long writerId,
 
         @Positive(message = "게시글의 아이디는 0이거나 음수일 수 없습니다.")
-        Long postId,
-
-        @EnumValue(enumClass = ChatType.class, message = "채팅 타입은 GARDEN(텃밭 분양)이거나 TRADE(작물거래)여야 합니다.")
-        String chatType
+        Long postId
 ) {
 
     public GardenChatRoomCreateParam to(LoginUser loginUser) {
         return new GardenChatRoomCreateParam(
                 writerId,
                 loginUser.memberId(),
-                postId,
-                ChatType.valueOf(chatType)
+                postId
         );
     }
 }
