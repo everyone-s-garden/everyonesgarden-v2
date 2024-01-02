fb31611eed66a10300ea4db2e4ec7754b4cae4ad
byeolhaha <j35635aa@gmail.com>
2024-01-03 01:42:56 +0900

    style : 채팅방 자원 분리에 따른 이름 변경

--- chat/src/main/java/com/garden/back/service/garden/GardenChatRoomService.java
+++ chat/src/main/java/com/garden/back/service/garden/GardenChatRoomService.java
@@ -1,6 +1,8 @@
 package com.garden.back.service;
 
 import com.garden.back.domain.ChatType;
+import com.garden.back.domain.crop.CropChatRoom;
+import com.garden.back.domain.crop.CropChatRoomInfo;
 import com.garden.back.domain.garden.GardenChatRoom;
 import com.garden.back.domain.garden.GardenChatRoomInfo;
 import com.garden.back.exception.ChatRoomAccessException;
@@ -9,6 +11,7 @@ import com.garden.back.repository.chatmessage.garden.GardenChatMessageRepository
 import com.garden.back.repository.chatroom.garden.GardenChatRoomRepository;
 import com.garden.back.repository.chatroominfo.garden.GardenChatRoomInfoRepository;
 import com.garden.back.service.dto.request.ChatRoomEntryParam;
+import com.garden.back.service.dto.request.CropChatRoomCreateParam;
 import com.garden.back.service.dto.request.GardenChatRoomCreateParam;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +27,7 @@ public class ChatRoomService {
     private final GardenChatRoomInfoRepository gardenChatRoomInfoRepository;
     private final ChatRoomEntryRepository chatRoomEntryRepository;
     private final GardenChatMessageRepository gardenChatMessageRepository;
+    private final CropChatRoomR
 
     public ChatRoomService(
             GardenChatRoomRepository gardenChatRoomRepository,
@@ -46,6 +50,23 @@ public class ChatRoomService {
 
         GardenChatRoomInfo chatRoomInfoToWriter = param.toChatRoomInfoToWriter();
         GardenChatRoomInfo chatRoomInfoToViewer = param.toChatRoomInfoToViewer();
+        chatRoomInfoToWriter.create(savedChatRoom);
+        chatRoomInfoToViewer.create(savedChatRoom);
+        gardenChatRoomInfoRepository.saveAll(List.of(chatRoomInfoToWriter, chatRoomInfoToViewer));
+
+        return savedChatRoom.getChatRoomId();
+    }
+
+    @Transactional
+    public Long createCropChatRoom(CropChatRoomCreateParam param) {
+        if (gardenChatRoomInfoRepository.existsByParams(param.toChatRoomCreateRepositoryParam())) {
+            throw new IllegalArgumentException("해당 게시글과 유저에 관한 채팅방은 이미 존재하여 새롭게 생성할 수 없습니다.");
+        }
+
+        CropChatRoom savedChatRoom = gardenChatRoomRepository.save(param.toChatRoom());
+
+        CropChatRoomInfo chatRoomInfoToWriter = param.toChatRoomInfoToWriter();
+        CropChatRoomInfo chatRoomInfoToViewer = param.toChatRoomInfoToViewer();
         chatRoomInfoToWriter.create(savedChatRoom);
         chatRoomInfoToViewer.create(savedChatRoom);
         gardenChatRoomInfoRepository.saveAll(List.of(chatRoomInfoToWriter, chatRoomInfoToViewer));
@@ -78,5 +99,7 @@ public class ChatRoomService {
                 param.roomId(),
                 partnerId);
     }
+
+
 
 }
