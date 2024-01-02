5e2b339d47a7a5ca30441066cb4f5c2d8afe6d7e
byeolhaha <j35635aa@gmail.com>
2024-01-03 01:49:54 +0900

    feat : 작물 거래 관련 채팅방 입장 컨트롤러 구현

--- chat/src/main/java/com/garden/back/controller/ChatRoomController.java
+++ chat/src/main/java/com/garden/back/controller/ChatRoomController.java
@@ -1,14 +1,19 @@
 package com.garden.back.controller;
 
+import com.garden.back.controller.dto.CropChatRoomCreateRequest;
 import com.garden.back.controller.dto.GardenChatRoomCreateRequest;
 import com.garden.back.global.LocationBuilder;
 import com.garden.back.global.loginuser.CurrentUser;
 import com.garden.back.global.loginuser.LoginUser;
-import com.garden.back.service.ChatRoomService;
+import com.garden.back.service.crop.CropChatRoomService;
+import com.garden.back.service.garden.GardenChatRoomService;
 import jakarta.validation.Valid;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
@@ -16,9 +21,11 @@ import java.net.URI;
 @RestController
 public class ChatRoomController {
 
-    private final ChatRoomService chatRoomService;
+    private final GardenChatRoomService gardenChatRoomService;
+    private final CropChatRoomService chatRoomService;
 
-    public ChatRoomController(ChatRoomService chatRoomService) {
+    public ChatRoomController(GardenChatRoomService gardenChatRoomService, CropChatRoomService chatRoomService) {
+        this.gardenChatRoomService = gardenChatRoomService;
         this.chatRoomService = chatRoomService;
     }
 
@@ -26,12 +33,27 @@ public class ChatRoomController {
             path = "/gardens",
             consumes = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<Void> createChatRoom(
+    public ResponseEntity<Void> createGardenChatRoom(
             @RequestBody @Valid GardenChatRoomCreateRequest request,
             @CurrentUser LoginUser loginUser
     ){
         URI location = LocationBuilder.buildLocation(
-                chatRoomService.createGardenChatRoom(request.to(loginUser)
+                gardenChatRoomService.createGardenChatRoom(request.to(loginUser)
+                ));
+
+        return ResponseEntity.created(location).build();
+    }
+
+    @PostMapping(
+            path = "/crops",
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Void> createCropChatRoom(
+            @RequestBody @Valid CropChatRoomCreateRequest request,
+            @CurrentUser LoginUser loginUser
+    ){
+        URI location = LocationBuilder.buildLocation(
+                chatRoomService.createCropChatRoom(request.to(loginUser)
                 ));
 
         return ResponseEntity.created(location).build();
