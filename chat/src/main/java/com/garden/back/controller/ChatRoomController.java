a26857bfa8f4251270e6abc5be68d1828d64dc89
byeolhaha <j35635aa@gmail.com>
2024-01-05 02:25:36 +0900

    feat : 텃밭 분양 채팅방 입장 컨트롤러 구현

--- chat/src/main/java/com/garden/back/controller/ChatRoomController.java
+++ chat/src/main/java/com/garden/back/controller/ChatRoomController.java
@@ -1,19 +1,21 @@
 package com.garden.back.controller;
 
-import com.garden.back.controller.dto.CropChatRoomCreateRequest;
-import com.garden.back.controller.dto.GardenChatRoomCreateRequest;
+import com.garden.back.controller.dto.request.CropChatRoomCreateRequest;
+import com.garden.back.controller.dto.request.GardenChatRoomCreateRequest;
+import com.garden.back.controller.dto.response.GardenChatRoomEnterResponse;
+import com.garden.back.facade.ChatRoomFacade;
+import com.garden.back.facade.dto.GardenChatRoomEnterFacadeRequest;
 import com.garden.back.global.LocationBuilder;
 import com.garden.back.global.loginuser.CurrentUser;
 import com.garden.back.global.loginuser.LoginUser;
 import com.garden.back.service.crop.CropChatRoomService;
 import com.garden.back.service.garden.GardenChatRoomService;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 
@@ -23,10 +25,12 @@ public class ChatRoomController {
 
     private final GardenChatRoomService gardenChatRoomService;
     private final CropChatRoomService chatRoomService;
+    private final ChatRoomFacade chatRoomFacade;
 
-    public ChatRoomController(GardenChatRoomService gardenChatRoomService, CropChatRoomService chatRoomService) {
+    public ChatRoomController(GardenChatRoomService gardenChatRoomService, CropChatRoomService chatRoomService, ChatRoomFacade chatRoomFacade) {
         this.gardenChatRoomService = gardenChatRoomService;
         this.chatRoomService = chatRoomService;
+        this.chatRoomFacade = chatRoomFacade;
     }
 
     @PostMapping(
@@ -36,7 +40,7 @@ public class ChatRoomController {
     public ResponseEntity<Void> createGardenChatRoom(
             @RequestBody @Valid GardenChatRoomCreateRequest request,
             @CurrentUser LoginUser loginUser
-    ){
+    ) {
         URI location = LocationBuilder.buildLocation(
                 gardenChatRoomService.createGardenChatRoom(request.to(loginUser)
                 ));
@@ -51,12 +55,27 @@ public class ChatRoomController {
     public ResponseEntity<Void> createCropChatRoom(
             @RequestBody @Valid CropChatRoomCreateRequest request,
             @CurrentUser LoginUser loginUser
-    ){
+    ) {
         URI location = LocationBuilder.buildLocation(
                 chatRoomService.createCropChatRoom(request.to(loginUser)
                 ));
 
         return ResponseEntity.created(location).build();
+    }
+
+    @PatchMapping(
+            path = "/gardens/{roomId}",
+            consumes = MediaType.APPLICATION_JSON_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<GardenChatRoomEnterResponse> enterGardenChatRoom(
+            @PathVariable @Positive Long roomId,
+            @CurrentUser LoginUser loginUser
+    ) {
+        GardenChatRoomEnterFacadeRequest request = GardenChatRoomEnterFacadeRequest.to(roomId, loginUser);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(GardenChatRoomEnterResponse.to(
+                        chatRoomFacade.enterGardenChatRoom(request)));
     }
 
 }
