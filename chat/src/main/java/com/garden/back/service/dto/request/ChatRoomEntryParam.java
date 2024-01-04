400a20369649997dbbf3c48091741f4ebccbeba7
byeolhaha <j35635aa@gmail.com>
2024-01-05 02:51:15 +0900

    refactor : static 메서드 추가

--- chat/src/main/java/com/garden/back/service/dto/request/ChatRoomEntryParam.java
+++ chat/src/main/java/com/garden/back/service/dto/request/ChatRoomEntryParam.java
@@ -1,10 +1,19 @@
 package com.garden.back.service.dto.request;
 
-import com.garden.back.domain.ChatType;
+import com.garden.back.global.loginuser.LoginUser;
 
 public record ChatRoomEntryParam (
         Long roomId,
-        Long memberId,
-        ChatType chatType
+        Long memberId
 ) {
+    public static ChatRoomEntryParam to(
+            Long roomId,
+            LoginUser loginUser
+    ){
+        return new ChatRoomEntryParam(
+                roomId,
+                loginUser.memberId()
+        );
+
+    }
 }
