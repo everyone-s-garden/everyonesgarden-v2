9824036744f4a204cfaa07280b3e8c992d1e7437
byeolhaha <j35635aa@gmail.com>
2024-01-08 16:55:07 +0900

    feat : 마이 페이지에서 회원 정보 조회 서비스 구현

--- core/src/main/java/com/garden/back/member/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.garden.back.member;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(String email);
-}
