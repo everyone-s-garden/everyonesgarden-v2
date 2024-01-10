62776588a03f6d6d9e3a092a0a847be597399c15
byeolhaha <j35635aa@gmail.com>
2024-01-10 14:30:37 +0900

    refactor : garden like 찜하기 도메인과 엔티티 분리에 따른 레파지토리 변경 및 합성으로 구조 변경

---
+++ core/src/main/java/com/garden/back/garden/repository/gardenlike/GardenLikeRepositoryImpl.java
@@ -0,0 +1,39 @@
+package com.garden.back.garden.repository.gardenlike;
+
+import com.garden.back.garden.domain.GardenLike;
+import com.garden.back.garden.repository.garden.GardenJpaRepository;
+import com.garden.back.garden.repository.gardenlike.entity.GardenLikeEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class GardenLikeRepositoryImpl implements GardenLikeRepository {
+
+    private final GardenLikeJpaRepository gardenLikeJpaRepository;
+    private final GardenJpaRepository gardenJpaRepository;
+
+    public GardenLikeRepositoryImpl(GardenLikeJpaRepository gardenLikeJpaRepository, GardenJpaRepository gardenJpaRepository) {
+        this.gardenLikeJpaRepository = gardenLikeJpaRepository;
+        this.gardenJpaRepository = gardenJpaRepository;
+    }
+
+    @Override
+    public GardenLike save(GardenLike gardenLike) {
+        return gardenLikeJpaRepository.save(GardenLikeEntity.from(gardenLike,
+                gardenJpaRepository.getById(gardenLike.getGarden().getGardenId()))).toModel();
+    }
+
+    @Override
+    public void delete(Long memberId, Long gardenId) {
+        gardenLikeJpaRepository.delete(memberId, gardenId);
+    }
+
+    @Override
+    public List<GardenLike> findAll() {
+        return gardenLikeJpaRepository.findAll().stream()
+                .map(GardenLikeEntity::toModel)
+                .toList();
+    }
+
+}
