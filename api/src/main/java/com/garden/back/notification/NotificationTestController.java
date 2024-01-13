3d2273118d1c2971baf9d351d13b18a3e660dc6a
Jinkyum Park <jinkyumpark@toss.im>
2024-01-13 21:22:15 +0900

    feat: (알림 시스템) refactor test controller

--- api/src/main/java/com/garden/back/notification/NotificationTestController.java
+++ api/src/main/java/com/garden/back/notification/NotificationTestController.java
@@ -4,23 +4,22 @@ import com.garden.back.notification.domain.slack.SlackChannel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Profile("local")
 @RequiredArgsConstructor
-@RequestMapping("test")
 @RestController
 public class NotificationTestController {
 
     private final NotificationApplication notificationApplication;
 
-    @PostMapping("email")
-    public void email() {
-        notificationApplication.toEmail("me@jinkyumpark.com", "TEST", "TEST");
+    @PostMapping("/test/email")
+    public void email(@RequestParam String to) {
+        notificationApplication.toEmail(to, "TEST", "TEST");
     }
 
-    @PostMapping("slack")
+    @PostMapping("/test/slack")
     public void slack() {
         notificationApplication.toSlack(SlackChannel.BOT, "TEST");
     }
