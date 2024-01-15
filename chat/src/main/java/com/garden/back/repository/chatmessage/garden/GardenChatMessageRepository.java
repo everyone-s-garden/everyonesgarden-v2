bcdf86e91589b3f870ca8ac07674d6d865416cd1
byeolhaha <j35635aa@gmail.com>
2024-01-15 17:08:03 +0900

    feat : 채팅 메세지 id로 찾는 레파지토리 메서드 추가

--- chat/src/main/java/com/garden/back/repository/chatmessage/garden/GardenChatMessageRepository.java
+++ chat/src/main/java/com/garden/back/repository/chatmessage/garden/GardenChatMessageRepository.java
@@ -3,6 +3,7 @@ package com.garden.back.repository.chatmessage.garden;
 import com.garden.back.domain.garden.GardenChatMessage;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface GardenChatMessageRepository {
 
@@ -11,4 +12,8 @@ public interface GardenChatMessageRepository {
     GardenChatMessage save(GardenChatMessage chatMessage);
 
     List<GardenChatMessage> findAll();
+
+    Optional<GardenChatMessage> findById(Long chatMessageId);
+
+    GardenChatMessage getById(Long chatMessageId);
 }
--- chat/src/main/java/com/garden/back/repository/chatmessage/garden/GardenChatMessageRepositoryImpl.java
+++ chat/src/main/java/com/garden/back/repository/chatmessage/garden/GardenChatMessageRepositoryImpl.java
@@ -1,9 +1,11 @@
 package com.garden.back.repository.chatmessage.garden;
 
 import com.garden.back.domain.garden.GardenChatMessage;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class GardenChatMessageRepositoryImpl implements GardenChatMessageRepository {
@@ -28,5 +30,15 @@ public class GardenChatMessageRepositoryImpl implements GardenChatMessageReposit
     @Override
     public List<GardenChatMessage> findAll() {
         return gardenChatMessageJpaRepository.findAll();
+    }
+
+    @Override
+    public Optional<GardenChatMessage> findById(Long chatMessageId) {
+        return gardenChatMessageJpaRepository.findById(chatMessageId);
+    }
+
+    @Override
+    public GardenChatMessage getById(Long chatMessageId) {
+        return findById(chatMessageId).orElseThrow(()-> new EntityNotFoundException("해당하는 아이디의 메세지는 존재하지 않습니다."));
     }
 }
