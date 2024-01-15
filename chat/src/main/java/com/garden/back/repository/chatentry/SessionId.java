12e8392dbb89f794dff109b51d61a95e7fa1a8a3
byeolhaha <j35635aa@gmail.com>
2024-01-15 17:08:35 +0900

    refactor : equals&hashCode 메서드 구현

--- chat/src/main/java/com/garden/back/repository/chatentry/SessionId.java
+++ chat/src/main/java/com/garden/back/repository/chatentry/SessionId.java
@@ -1,5 +1,7 @@
 package com.garden.back.repository.chatentry;
 
+import java.util.Objects;
+
 public class SessionId {
 
     private final String sessionId;
@@ -12,5 +14,18 @@ public class SessionId {
         return new SessionId(
                 sessionId
         );
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SessionId sessionId1 = (SessionId) o;
+        return Objects.equals(sessionId, sessionId1.sessionId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(sessionId);
     }
 }
