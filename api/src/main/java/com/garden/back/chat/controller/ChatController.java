20b619bcae3a5bdd574ac369d4be7c172cce5ae9
byeolhaha <j35635aa@gmail.com>
2024-01-17 14:58:16 +0900

    feat : 채팅방 메세지 전송 컨트롤러 구현

---
+++ api/src/main/java/com/garden/back/chat/controller/ChatController.java
@@ -0,0 +1,32 @@
+package com.garden.back.chat.controller;
+
+import com.garden.back.chat.controller.dto.request.GardenMessageSendRequest;
+import com.garden.back.chat.controller.dto.response.GardenMessageSendResponse;
+import com.garden.back.global.loginuser.CurrentUser;
+import com.garden.back.global.loginuser.LoginUser;
+import com.garden.back.service.garden.GardenChatService;
+import jakarta.validation.Valid;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.annotation.SendToUser;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ChatController {
+
+    private final GardenChatService gardenChatService;
+
+    public ChatController(GardenChatService gardenChatService) {
+        this.gardenChatService = gardenChatService;
+    }
+
+    @MessageMapping("/chats/{roomId}/messages")
+    @SendToUser("/queue/chats/{roomId}")
+    public GardenMessageSendResponse sendMessage(@DestinationVariable("roomId") Long roomId,
+                                                 @CurrentUser LoginUser loginUser,
+                                                 @Payload @Valid GardenMessageSendRequest request) {
+        return GardenMessageSendResponse.to(gardenChatService.saveMessage(request.to(loginUser, roomId)));
+    }
+
+}
