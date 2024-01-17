a1ce6e755538e07a05ee08901356dc4f46b01d4d
byeolhaha <j35635aa@gmail.com>
2024-01-17 16:09:51 +0900

    refactor : 채팅방 정보 레파지토리 구조 변경 및 삭제에 대한 쿼리 추가

--- chat/src/main/java/com/garden/back/repository/chatroominfo/garden/GardenChatRoomInfoJpaRepository.java
+++ chat/src/main/java/com/garden/back/repository/chatroominfo/garden/GardenChatRoomInfoJpaRepository.java
@@ -4,6 +4,7 @@ import com.garden.back.domain.garden.GardenChatRoomInfo;
 import com.garden.back.repository.chatroom.dto.ChatRoomCreateRepositoryParam;
 import com.garden.back.repository.chatroominfo.garden.dto.GardenChatRoomEnterRepositoryResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -11,13 +12,13 @@ import java.util.List;
 
 public interface GardenChatRoomInfoJpaRepository extends JpaRepository<GardenChatRoomInfo, Long> {
     @Query("""
-           select case when exists(
-                    select cri
-                    from GardenChatRoomInfo as cri
-                    where cri.postId = :#{#param.postId}
-                      and cri.memberId = :#{#param.writerId}
-           ) then true else false end
-           """)
+            select case when exists(
+                     select cri
+                     from GardenChatRoomInfo as cri
+                     where cri.postId = :#{#param.postId}
+                       and cri.memberId = :#{#param.writerId}
+            ) then true else false end
+            """)
     boolean existsByParams(@Param("param") ChatRoomCreateRepositoryParam param);
 
     @Query(
@@ -42,5 +43,11 @@ public interface GardenChatRoomInfoJpaRepository extends JpaRepository<GardenCha
     )
     List<GardenChatRoomInfo> findByRoomId(@Param("chatRoomId") Long chatRoomId);
 
+    @Modifying(clearAutomatically = true)
+    @Query("""
+            delete from GardenChatRoomInfo as gri
+            where gri.chatRoom.chatRoomId =:chatRoomId
+            """)
+    void deleteAll(@Param("chatRoomId") Long chatRoomId);
 
 }
--- chat/src/main/java/com/garden/back/repository/chatroominfo/garden/GardenChatRoomInfoRepository.java
+++ chat/src/main/java/com/garden/back/repository/chatroominfo/garden/GardenChatRoomInfoRepository.java
@@ -1,4 +1,23 @@
 package com.garden.back.repository.chatroominfo.garden;
 
-public interface GardenChatRoomInfoRepository extends GardenChatRoomInfoJpaRepository {
+import com.garden.back.domain.garden.GardenChatRoomInfo;
+import com.garden.back.repository.chatroom.dto.ChatRoomCreateRepositoryParam;
+import com.garden.back.repository.chatroominfo.garden.dto.GardenChatRoomEnterRepositoryResponse;
+
+import java.util.List;
+
+public interface GardenChatRoomInfoRepository {
+
+    boolean existsByParams(ChatRoomCreateRepositoryParam param);
+
+    GardenChatRoomEnterRepositoryResponse findPartnerId(Long chatRoomId,
+                                                        Long memberId);
+
+    List<GardenChatRoomInfo> findByRoomId(Long chatRoomId);
+
+    void deleteAll(Long chatRoomId);
+
+    List<GardenChatRoomInfo> saveAll(List<GardenChatRoomInfo> gardenChatRoomInfos);
+
+    List<GardenChatRoomInfo> findAll();
 }
