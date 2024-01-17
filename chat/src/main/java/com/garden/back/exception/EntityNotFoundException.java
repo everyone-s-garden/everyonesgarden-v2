f08c1f048fef6e85398177ff6b7b3de644dd805f
byeolhaha <j35635aa@gmail.com>
2024-01-17 15:26:37 +0900

    feat : 존재하지 않는 자원에 대한 커스텀 exception 구현

---
+++ chat/src/main/java/com/garden/back/exception/EntityNotFoundException.java
@@ -0,0 +1,15 @@
+package com.garden.back.exception;
+
+import com.garden.back.global.exception.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class EntityNotFoundException extends RuntimeException {
+    private final ErrorCode errorCode;
+
+    public EntityNotFoundException(final ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+}
--- chat/src/main/java/com/garden/back/global/exception/ErrorCode.java
+++ chat/src/main/java/com/garden/back/global/exception/ErrorCode.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 public enum ErrorCode{
     //global
     INTERNAL_SERVER_ERROR("G001", "Internal Server Error"),
+    NOT_FOUND_ENTITY("G002","해당하는 자원은 존재하지 않습니다."),
 
     CHAT_ROOM_ACCESS_ERROR("C001","해당 사용자는 이 채팅방에 소속되어 있지 않습니다."),
     CHAT_ROOM_SESSION_ACCESS_ERROR("C002","해당 사용자는 이 세션에 소속되어 있지 않습니다.");
