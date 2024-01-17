d0dbece1249b8fb719d5cffaae9460927ad535ae
byeolhaha <j35635aa@gmail.com>
2024-01-17 16:37:43 +0900

    feat : 채팅방 나가기 이벤트 리스너 구현

--- api/src/main/java/com/garden/back/chat/controller/ChatController.java
+++ api/src/main/java/com/garden/back/chat/controller/ChatController.java
@@ -4,13 +4,16 @@ import com.garden.back.chat.controller.dto.request.GardenMessageSendRequest;
 import com.garden.back.chat.controller.dto.response.GardenMessageSendResponse;
 import com.garden.back.global.loginuser.CurrentUser;
 import com.garden.back.global.loginuser.LoginUser;
+import com.garden.back.repository.chatentry.SessionId;
 import com.garden.back.service.garden.GardenChatService;
 import jakarta.validation.Valid;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 @RestController
 public class ChatController {
@@ -27,6 +30,11 @@ public class ChatController {
                                                  @CurrentUser LoginUser loginUser,
                                                  @Payload @Valid GardenMessageSendRequest request) {
         return GardenMessageSendResponse.to(gardenChatService.saveMessage(request.to(loginUser, roomId)));
+    }
+
+    @EventListener
+    public void onDisconnectEvent(SessionDisconnectEvent event) {
+        gardenChatService.leaveChatRoom(SessionId.of(event.getSessionId()));
     }
 
 }
