adbcfe1ace8795e685130834c68244feb0baf1c5
byeolhaha <j35635aa@gmail.com>
2024-01-19 00:19:39 +0900

    refactor : jpaconfig 추가

---
+++ chat/src/main/java/com/garden/back/global/JpaConfig.java
@@ -0,0 +1,16 @@
+package com.garden.back.global;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+    @PostConstruct
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
+}
