a7b5e1e1d407181732af7e4705ba1e11af6b36f1
byeolhaha <j35635aa@gmail.com>
2024-01-29 17:21:44 +0900

    feat : 채팅방 신고 관련 레파지토리 구현

---
+++ chat/src/main/java/com/garden/back/garden/repository/chatreport/image/GardenChatReportImageJpaRepository.java
@@ -0,0 +1,7 @@
+package com.garden.back.garden.repository.chatreport.image;
+
+import com.garden.back.garden.domain.GardenChatReportImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GardenChatReportImageJpaRepository extends JpaRepository<GardenChatReportImage, Long> {
+}
