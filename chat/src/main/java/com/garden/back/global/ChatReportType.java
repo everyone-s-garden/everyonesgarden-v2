6083f6c080c1441353052def37ec6e0601f98e9b
byeolhaha <j35635aa@gmail.com>
2024-01-29 17:20:30 +0900

    feat : 채팅방 신고 항목 enum 구현

---
+++ chat/src/main/java/com/garden/back/global/ChatReportType.java
@@ -0,0 +1,17 @@
+package com.garden.back.global;
+
+public enum ChatReportType {
+    NON_MANNER_USER("비매너사용자", 1),
+    DISPUTE("분쟁", 3),
+    FRAUD("사기", 5),
+    SWEAR_WORD("욕설", 2),
+    INAPPROPRIATE_BEHAVIOR("부적절한 행위", 3);
+
+    private final String description;
+    private final int score;
+
+    ChatReportType(String description, int score) {
+        this.description = description;
+        this.score = score;
+    }
+}
