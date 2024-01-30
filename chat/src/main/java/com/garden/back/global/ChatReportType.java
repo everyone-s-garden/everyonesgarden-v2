c523fe003940c6379eb5ea8985d8c58706b62d30
byeolhaha <j35635aa@gmail.com>
2024-01-30 16:28:52 +0900

    refactor : 신고 점수 수정 및 getter 추가

--- chat/src/main/java/com/garden/back/global/ChatReportType.java
+++ chat/src/main/java/com/garden/back/global/ChatReportType.java
@@ -1,11 +1,14 @@
 package com.garden.back.global;
 
+import lombok.Getter;
+
+@Getter
 public enum ChatReportType {
-    NON_MANNER_USER("비매너사용자", 1),
-    DISPUTE("분쟁", 3),
-    FRAUD("사기", 5),
-    SWEAR_WORD("욕설", 2),
-    INAPPROPRIATE_BEHAVIOR("부적절한 행위", 3);
+    NON_MANNER_USER("비매너사용자", -1),
+    DISPUTE("분쟁", -3),
+    FRAUD("사기", -10),
+    SWEAR_WORD("욕설", -2),
+    INAPPROPRIATE_BEHAVIOR("부적절한 행위", -3);
 
     private final String description;
     private final int score;
