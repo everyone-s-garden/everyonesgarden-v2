b56b14f0bc5f41a8625ea9cbddb6f30550bedd84
joyfulviper <kimsk3113@naver.com>
2024-02-01 19:12:26 +0900

    fix: kotlin 리플렉션 모듈 추가

--- core/src/main/java/com/garden/back/notification/entity/NotificationRepository.kt
+++ core/src/main/java/com/garden/back/notification/entity/NotificationRepository.kt
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface NotificationRepository : JpaRepository<NotificationEntity, Long> {
+    //fun findAllByMemberId(memberId: Long): List<NotificationEntity>
     @Query("select n from NotificationEntity n where n.memberId = ?1")
     fun findAllByMemberId(memberId: Long): List<NotificationEntity>
 
+    //fun findAllByMemberIdAndIsReadIsFalse(memberId: Long): List<NotificationEntity>
     @Query("select noti from NotificationEntity noti where noti.memberId = :memberId and noti.isRead = false")
     fun findAllUnread(memberId: Long): List<NotificationEntity>
 }
