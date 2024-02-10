99c2c85637a77952fe036c76bda4f1832880de11
byeolhaha <j35635aa@gmail.com>
2024-02-10 23:29:29 +0900

    style : 채팅방 입장할 때 상대방 등급, 프로필 필드 추가

--- api/src/main/java/com/garden/back/chat/gardenchat/controller/dto/response/GardenChatRoomEnterResponse.java
+++ api/src/main/java/com/garden/back/chat/gardenchat/controller/dto/response/GardenChatRoomEnterResponse.java
@@ -5,23 +5,27 @@ import com.garden.back.chat.gardenchat.facade.GardenChatRoomEnterFacadeResponse;
 import java.util.List;
 
 public record GardenChatRoomEnterResponse(
-        Long partnerId,
-        String partnerNickname,
-        Long postId,
-        String gardenStatus,
-        String gardenName,
-        String price,
-        List<String> images
+    Long partnerId,
+    String partnerNickname,
+    String partnerMannerGrade,
+    String partnerProfileImage,
+    Long postId,
+    String gardenStatus,
+    String gardenName,
+    String price,
+    List<String> images
 ) {
-    public static GardenChatRoomEnterResponse to(GardenChatRoomEnterFacadeResponse response){
+    public static GardenChatRoomEnterResponse to(GardenChatRoomEnterFacadeResponse response) {
         return new GardenChatRoomEnterResponse(
-                response.partnerId(),
-                response.partnerNickname(),
-                response.postId(),
-                response.gardenStatus(),
-                response.gardenName(),
-                response.price(),
-                response.images()
+            response.partnerId(),
+            response.partnerNickname(),
+            response.partnerMannerGrade(),
+            response.partnerProfileImage(),
+            response.postId(),
+            response.gardenStatus(),
+            response.gardenName(),
+            response.price(),
+            response.images()
         );
     }
 
