b295873a26efba03b3c99f2a2d806410de591355
joyfulviper <kimsk3113@naver.com>
2024-03-06 00:05:47 +0900

    refactor: email 응답 필드 추가

--- api/src/main/java/com/garden/back/member/dto/MemberMyPageResponse.java
+++ api/src/main/java/com/garden/back/member/dto/MemberMyPageResponse.java
@@ -5,13 +5,15 @@ import com.garden.back.member.service.dto.MemberMyPageResult;
 public record MemberMyPageResponse(
     String nickname,
     String profileImage,
-    String memberMannerGrade
+    String memberMannerGrade,
+    String email
 ) {
     public static MemberMyPageResponse to(MemberMyPageResult result) {
         return new MemberMyPageResponse(
             result.nickname(),
             result.imageUrl(),
-            result.memberMannerGrade()
+            result.memberMannerGrade(),
+            result.email()
         );
     }
 }
