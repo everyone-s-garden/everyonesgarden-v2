997c861980aa004a74f6816cb4ce5a70ac3b2947
byeolhaha <j35635aa@gmail.com>
2024-07-30 00:29:38 +0900

    refactor : 부대시설 입력 요구사항 변경으로 인한 로직 수정

--- core/src/main/java/com/garden/back/garden/service/dto/request/GardenCreateParam.java
+++ core/src/main/java/com/garden/back/garden/service/dto/request/GardenCreateParam.java
@@ -2,55 +2,72 @@ package com.garden.back.garden.service.dto.request;
 
 import com.garden.back.garden.domain.Garden;
 import com.garden.back.garden.domain.vo.GardenStatus;
+import com.garden.back.garden.domain.vo.GardenType;
 import com.garden.back.global.GeometryUtil;
+import org.locationtech.jts.geom.Point;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public record GardenCreateParam(
-        List<MultipartFile> gardenImages,
-        String gardenName,
-        String price,
-        String size,
-        GardenStatus gardenStatus,
-        String contact,
-        String address,
-        Double latitude,
-        Double longitude,
-        GardenFacility gardenFacility,
-        String gardenDescription,
-        LocalDate recruitStartDate,
-        LocalDate recruitEndDate,
-        Long writerId
+    List<MultipartFile> gardenImages,
+    String gardenName,
+    String price,
+    String size,
+    GardenStatus gardenStatus,
+    String contact,
+    String address,
+    Double latitude,
+    Double longitude,
+    GardenFacility gardenFacility,
+    String gardenDescription,
+    LocalDate recruitStartDate,
+    LocalDate recruitEndDate,
+    Long writerId
 
 ) {
-    public static Garden toEntity(GardenCreateParam param ){
-        return Garden.createPrivateGarden(
-                param.address(),
-                param.latitude(),
-                param.longitude(),
-                GeometryUtil.createPoint(param.latitude(),param.longitude()),
-                param.gardenName(),
-                param.gardenStatus(),
-                param.price(),
-                param.contact(),
-                param.size(),
-                param.gardenDescription(),
-                param.recruitStartDate(),
-                param.recruitEndDate(),
-                param.gardenFacility().isToilet(),
-                param.gardenFacility().isWaterway(),
-                param.gardenFacility().isEquipment(),
-                param.writerId()
+    public Garden toEntity() {
+        return Garden.createGarden(
+            address,
+            latitude,
+            longitude,
+            GeometryUtil.createPoint(latitude, longitude),
+            GardenType.PRIVATE,
+            gardenName,
+            gardenStatus,
+            price,
+            contact,
+            size,
+            gardenDescription,
+            recruitStartDate.toString(),
+            recruitEndDate.toString(),
+            gardenFacility.getGardenFacilities(),
+            writerId
         );
     }
-
     public record GardenFacility(
-            Boolean isToilet,
-            Boolean isWaterway,
-            Boolean isEquipment
-    ){
+        Boolean isToilet,
+        Boolean isWaterway,
+        Boolean isEquipment
+    ) {
 
+        private String getGardenFacilities() {
+            StringBuilder sb = new StringBuilder();
+            if (Boolean.TRUE.equals(isToilet)) {
+                sb.append("화장실");
+            }
+            if (Boolean.TRUE.equals(isEquipment)) {
+                if (!sb.isEmpty()) sb.append(", ");
+                sb.append("농기구");
+            }
+            if (Boolean.TRUE.equals(isWaterway)) {
+                if (!sb.isEmpty()) sb.append(", ");
+                sb.append("수로");
+            }
+
+            return sb.toString();
+        }
     }
+
 }
--- core/src/main/java/com/garden/back/garden/service/dto/request/GardenUpdateParam.java
+++ core/src/main/java/com/garden/back/garden/service/dto/request/GardenUpdateParam.java
@@ -8,52 +8,66 @@ import org.springframework.web.multipart.MultipartFile;
 import java.time.LocalDate;
 import java.util.List;
 
-public record GardenUpdateParam (
-        Long gardenId,
-        List<String> remainGardenImageUrls,
-        List<MultipartFile> newGardenImages,
-        String gardenName,
-        String price,
-        String size,
-        GardenStatus gardenStatus,
-        GardenType gardenType,
-        String contact,
-        String address,
-        Double latitude,
-        Double longitude,
-        GardenUpdateParam.GardenFacility gardenFacility,
-        String gardenDescription,
-        LocalDate recruitStartDate,
-        LocalDate recruitEndDate,
-        Long writerId
+public record GardenUpdateParam(
+    Long gardenId,
+    List<String> remainGardenImageUrls,
+    List<MultipartFile> newGardenImages,
+    String gardenName,
+    String price,
+    String size,
+    GardenStatus gardenStatus,
+    GardenType gardenType,
+    String contact,
+    String address,
+    Double latitude,
+    Double longitude,
+    GardenUpdateParam.GardenFacility gardenFacility,
+    String gardenDescription,
+    LocalDate recruitStartDate,
+    LocalDate recruitEndDate,
+    Long writerId
 
 ) {
     public GardenUpdateDomainRequest toGardenUpdateDomainRequest() {
         return new GardenUpdateDomainRequest(
-                gardenName,
-                price,
-                size,
-                gardenStatus,
-                gardenType,
-                contact,
-                address,
-                latitude,
-                longitude,
-                gardenFacility().isToilet,
-                gardenFacility().isWaterway,
-                gardenFacility().isEquipment,
-                gardenDescription,
-                recruitStartDate,
-                recruitEndDate,
-                writerId
+            gardenName,
+            price,
+            size,
+            gardenStatus,
+            gardenType,
+            contact,
+            address,
+            latitude,
+            longitude,
+            gardenFacility.getGardenFacilities(),
+            gardenDescription,
+            recruitStartDate.toString(),
+            recruitEndDate.toString(),
+            writerId
         );
     }
 
     public record GardenFacility(
-            boolean isToilet,
-            boolean isWaterway,
-            boolean isEquipment
+        boolean isToilet,
+        boolean isWaterway,
+        boolean isEquipment
     ) {
+        public String getGardenFacilities() {
+            StringBuilder sb = new StringBuilder();
+            if (Boolean.TRUE.equals(isToilet)) {
+                sb.append("화장실");
+            }
+            if (Boolean.TRUE.equals(isEquipment)) {
+                if (!sb.isEmpty()) sb.append(", ");
+                sb.append("농기구");
+            }
+            if (Boolean.TRUE.equals(isWaterway)) {
+                if (!sb.isEmpty()) sb.append(", ");
+                sb.append("수로");
+            }
+
+            return sb.toString();
+        }
 
     }
 }
