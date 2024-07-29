1e1a79d8462a5dbf11a2cdbd988e9999ccf6ee10
byeolhaha <j35635aa@gmail.com>
2024-07-30 00:39:33 +0900

    feat : 서울 시간 기준 새벽 3시 작동하는 텃밭 공공데이터 적재 배치 구현

--- batch/src/main/java/com/garden/back/garden/OpenAPIGardenInfoClient.java
+++ batch/src/main/java/com/garden/back/garden/OpenAPIGardenInfoClient.java
@@ -10,5 +10,5 @@ import org.springframework.web.bind.annotation.GetMapping;
 public interface OpenAPIGardenInfoClient {
 
     @GetMapping
-    FarmResponse getFarmFromOpnAPI();
+    FarmResponse getFarmFromOpenAPI();
 }
