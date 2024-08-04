b5166108f273d356ce94baeaf9de0a11bda20137
joyfulviper <kimsk3113@naver.com>
2024-08-04 11:32:19 +0900

    누락된 배포 스크립트 설정들 추가

--- a-cicd/code-deploy/app-start.sh
+++ a-cicd/code-deploy/app-start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# NGINX 설정에서 서비스 URL을 가져옴
 service_url=$(grep "set \$service_url" /etc/nginx/conf.d/service-url.inc | cut -d ':' -f 3)
 
 # 포트 번호에 따라 IDLE_PROFILE 값을 설정
@@ -17,21 +18,25 @@ else
 fi
 echo "배포 후 실행 될 프로필과 포트: $IDLE_PROFILE, $IDLE_PORT"
 
+# JAR 파일 경로 설정
 REPOSITORY=/home/ec2-user/everyonesgarden
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 echo "> JAR NAME: $JAR_NAME"
 
-echo "> $JAR_NAME 에 실행권한 추가"
-
+# JAR 파일 실행 권한 추가
 chmod +x $JAR_NAME
 
-echo "> $JAR_NAME 실행"
+# API_RUN_SCRIPT 파일 경로 설정
+API_RUN_SCRIPT=${REPOSITORY}/api-run.sh
 
-nohup java -jar \
--Dspring.profiles.active=$IDLE_PROFILE \
--Dspring.config.location=classpath:application.yml
-$JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+# API_RUN_SCRIPT 파일 생성 및 실행 내용 작성
+echo "nohup java -jar -Dspring.profiles.active=${IDLE_PROFILE} ${JAR_NAME} 1> /dev/null 2>&1 &" > ${API_RUN_SCRIPT}
+chmod 755 ${API_RUN_SCRIPT}
 
+# API_RUN_SCRIPT 실행
+${API_RUN_SCRIPT}
+
+# 헬스체크
 echo "> Jar 파일 실행 후 헬스체크"
 echo "> IDLE_PORT: $IDLE_PORT"
 echo "> curl -s http://localhost:$IDLE_PORT/profile "
@@ -41,11 +46,37 @@ for RETRY_COUNT in {1..10}
 do
   RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
   UP_COUNT=$(echo ${RESPONSE} | grep 'prod' | wc -l)
-  if [ ${UP_COUNT} -ge 1 ]
-  then # $up_count >= 1 ("prod" 문자열이 있는지 검증)
+  if [ ${UP_COUNT} -ge 1 ]; then
     echo "> Health check 성공"
     break
   else
-    echo "헬스체크 실패"
-    echo"헬스체크: ${RESPONSE}"
+    echo "> 헬스체크 실패"
+    echo "> 헬스체크: ${RESPONSE}"
   fi
+
+  if [ ${RETRY_COUNT} -eq 10]; then
+    echo "> Health check 실패."
+    echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
+    exit 1
+  fi
+
+  echo "> 헬스체크 실패. 10초 후 재시도"
+  sleep 10
+done
+
+# Nginx 포트 전환
+echo "> Port 전환"
+echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+echo "> Nginx Reload"
+sudo service nginx reload
+
+# 이전 포트 서비스 종료
+echo "이전 포트 죽이기"
+KILL_PID=$(lsof -ti tcp:${KILL_PORT})
+
+if [ ! -z "$KILL_PID" ]; then
+  echo "> 포트 ${KILL_PORT}에서 실행 중인 서비스 종료"
+  kill $KILL_PID
+else
+  echo "> 포트 ${KILL_PORT}에 실행 중인 서비스가 없습니다."
+fi
